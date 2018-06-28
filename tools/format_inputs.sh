cdcc0f31bdff7875ac71a6548b1d544e5972f090
Andreas Beger <adbeger@gmail.com>
2018-06-28 09:43:53 +0300

    add updated input files

--- tools/format_inputs.sh
+++ tools/format_inputs.sh
@@ -6,3 +6,10 @@ do
   python3 -m json.tool $f > temp.json
   mv temp.json $f
 done
+
+for f in tests/io/ex_*_input.json
+do
+  echo "$f"
+  python3 -m json.tool $f > temp.json
+  mv temp.json $f
+done
